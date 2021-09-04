2b97fdbf8eec62d2b879f5386b47b2a454f6f2d8
alXDraghiciu <88316605+alXDraghiciu@users.noreply.github.com>
2021-09-04 12:08:25 +0300

    Create Result.vue

---
+++ src/ui/Result.vue
@@ -0,0 +1,30 @@
+<template
+  ><div>
+    <img :src="course.img" />
+    <div>
+      <h3>{{ course.title }}</h3>
+      <p>{{ course.headline }}</p>
+      <div id="instructor-prices">
+        <span>{{ course.price }}</span>
+        <span>
+          <img :src="course.instructor - image" />
+          <h5>{{ course.instructor - name }}</h5>
+          <h6>{{ course.instructor - title }}</h6>
+        </span>
+      </div>
+    </div>
+  </div></template
+>
+
+<script>
+export default {
+  props: {
+    course: {
+      required: true,
+      type: Object,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
