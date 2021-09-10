6d2f704a0ac48a2480333b0c81be22bcb7cf378d
DorinSuciu <git@github.com:mascalucian/fe-finalproject.git>
2021-09-10 17:45:58 +0300

    Created Review component

---
+++ src/ui/Review.vue
@@ -0,0 +1,39 @@
+<template>
+    <div id="review">
+      <h3>Test</h3>
+      <p>
+          &nbsp;&nbsp;Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis imperdiet sem at enim molestie, eget faucibus metus tempor. Praesent aliquet ac eros sed suscipit. Duis in pellentesque turpis. Aenean placerat nisi vitae felis maximus, at mollis justo commodo. Duis nec dapibus augue. Donec tincidunt, nibh ut egestas luctus, erat neque vestibulum est, eget elementum nulla tortor at elit. Morbi pretium suscipit faucibus. Cras id magna in orci lacinia efficitur quis at urna. Vivamus vel nibh luctus, tristique sem eget, elementum tortor. Sed eget orci sed lacus volutpat malesuada.
+      </p>
+    </div>
+  </template>
+
+  <style scoped lang="scss">
+      
+  #review{
+    width: 100%;
+    height: auto;
+    margin-bottom: 1em;
+    border-radius: 10px;
+    background-color: white;
+    text-decoration: none;
+    border-radius: 10px;
+    box-shadow: 0px 1px 5px #888888;
+    color: black;
+     &:hover,
+     :active,
+     :focus-visible {
+        background-color: #d1d7dc;
+    }
+
+    h3{
+        padding: 0.75em;
+        margin: 0;
+    }
+    p{
+        padding: 0.75em;
+        margin: 0;
+        text-align:justify;
+    }
+
+}
+  </style>
