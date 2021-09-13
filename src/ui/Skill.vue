c85203f02bc0dfec5e88a8054859da1f952a9e54
DorinSuciu <git@github.com:mascalucian/fe-finalproject.git>
2021-09-13 22:24:30 +0300

    aligned edit icon + removed pascal case conversion

--- src/ui/Skill.vue
+++ src/ui/Skill.vue
@@ -13,7 +13,12 @@
       ></i>
     </div>
     <div id="route">
-      <i class="fab" :class="getFaIcon(skillName)" @click.stop="goToSkill(skill)"> </i>
+      <i
+        class="fab"
+        :class="getFaIcon(skillName)"
+        @click.stop="goToSkill(skill)"
+      >
+      </i>
       <div id="skill-name" class="event">
         <input
           ref="nameInput"
@@ -25,13 +30,13 @@
         />
         <h2 v-else>
           {{ skillName }}
-          <i
-            v-if="!edit"
-            class="fas fa-pen fa-lg"
-            @click="startEdit()"
-            :style="[!showButtons ? { visibility: 'hidden' } : '']"
-          ></i>
         </h2>
+        <i
+          v-if="!edit"
+          class="fas fa-pen fa-lg"
+          @click="startEdit()"
+          :style="[!showButtons ? { visibility: 'hidden' } : '']"
+        ></i>
         <i
           v-if="edit"
           id="save-button"
@@ -91,46 +96,69 @@ export default {
     },
     getFaIcon(name) {
       let filterName = name.toUpperCase();
-      if (filterName.indexOf("JAVASCRIPT") > -1 || filterName.indexOf("JS") > -1)
+      if (
+        filterName.indexOf("JAVASCRIPT") > -1 ||
+        filterName.indexOf("JS") > -1
+      )
         return "fa-js";
       else if (filterName.indexOf("java".toUpperCase()) > -1) return "fa-java";
-      else if (filterName.indexOf("amazong".toUpperCase()) > -1) return "fa-amazon";
-      else if (filterName.indexOf("angular".toUpperCase()) > -1) return "fa-angular";
-      else if (filterName.indexOf("app store".toUpperCase()) > -1) return "fa-app-store";
+      else if (filterName.indexOf("amazong".toUpperCase()) > -1)
+        return "fa-amazon";
+      else if (filterName.indexOf("angular".toUpperCase()) > -1)
+        return "fa-angular";
+      else if (filterName.indexOf("app store".toUpperCase()) > -1)
+        return "fa-app-store";
       else if (
         filterName.indexOf("apple".toUpperCase()) > -1 ||
         filterName.indexOf("mac".toUpperCase()) > -1
       )
         return "fa-apple";
-      else if (filterName.indexOf("atlassian".toUpperCase()) > -1) return "fa-atlassian";
-      else if (filterName.indexOf("bitbucket".toUpperCase()) > -1) return "fa-bitbucket";
-      else if (filterName.indexOf("bootstrap".toUpperCase()) > -1) return "fa-bootstrap";
-      else if (filterName.indexOf("css".toUpperCase()) > -1) return "fa-css3-alt";
-      else if (filterName.indexOf("docker".toUpperCase()) > -1) return "fa-docker";
-      else if (filterName.indexOf("figma".toUpperCase()) > -1) return "fa-figma";
-      else if (filterName.indexOf("github".toUpperCase()) > -1) return "fa-github";
-      else if (filterName.indexOf("gitkraken".toUpperCase()) > -1) return "fa-gitkraken";
-      else if (filterName.indexOf("gitlab".toUpperCase()) > -1) return "fa-gitlab";
-      else if (filterName.indexOf("git".toUpperCase()) > -1) return "fa-git-alt";
+      else if (filterName.indexOf("atlassian".toUpperCase()) > -1)
+        return "fa-atlassian";
+      else if (filterName.indexOf("bitbucket".toUpperCase()) > -1)
+        return "fa-bitbucket";
+      else if (filterName.indexOf("bootstrap".toUpperCase()) > -1)
+        return "fa-bootstrap";
+      else if (filterName.indexOf("css".toUpperCase()) > -1)
+        return "fa-css3-alt";
+      else if (filterName.indexOf("docker".toUpperCase()) > -1)
+        return "fa-docker";
+      else if (filterName.indexOf("figma".toUpperCase()) > -1)
+        return "fa-figma";
+      else if (filterName.indexOf("github".toUpperCase()) > -1)
+        return "fa-github";
+      else if (filterName.indexOf("gitkraken".toUpperCase()) > -1)
+        return "fa-gitkraken";
+      else if (filterName.indexOf("gitlab".toUpperCase()) > -1)
+        return "fa-gitlab";
+      else if (filterName.indexOf("git".toUpperCase()) > -1)
+        return "fa-git-alt";
       else if (filterName.indexOf("html".toUpperCase()) > -1) return "fa-html5";
-      else if (filterName.indexOf("jenkins".toUpperCase()) > -1) return "fa-jenkins";
+      else if (filterName.indexOf("jenkins".toUpperCase()) > -1)
+        return "fa-jenkins";
       else if (filterName.indexOf("jira".toUpperCase()) > -1) return "fa-jira";
       else if (filterName.indexOf("less".toUpperCase()) > -1) return "fa-less";
       else if (filterName.indexOf("node".toUpperCase()) > -1) return "fa-node";
       else if (filterName.indexOf("php".toUpperCase()) > -1) return "fa-php";
-      else if (filterName.indexOf("python".toUpperCase()) > -1) return "fa-python";
+      else if (filterName.indexOf("python".toUpperCase()) > -1)
+        return "fa-python";
       else if (filterName.indexOf("raspberry pi".toUpperCase()) > -1)
         return "fa-raspberry-pi";
-      else if (filterName.indexOf("react".toUpperCase()) > -1) return "fa-react";
+      else if (filterName.indexOf("react".toUpperCase()) > -1)
+        return "fa-react";
       else if (filterName.indexOf("rust".toUpperCase()) > -1) return "fa-rust";
       else if (filterName.indexOf("sass".toUpperCase()) > -1) return "fa-sass";
       else if (filterName.indexOf("sourcetree".toUpperCase()) > -1)
         return "fa-sourcetree";
-      else if (filterName.indexOf("unity".toUpperCase()) > -1) return "fa-unity";
+      else if (filterName.indexOf("unity".toUpperCase()) > -1)
+        return "fa-unity";
       else if (filterName.indexOf("vue".toUpperCase()) > -1) return "fa-vuejs";
-      else if (filterName.indexOf("wordpress".toUpperCase()) > -1) return "fa-wordpress";
-      else if (filterName.indexOf("sql".toUpperCase()) > -1) return "fas fa-database";
-      else if (filterName.indexOf("backend".toUpperCase()) > -1) return "fas fa-terminal";
+      else if (filterName.indexOf("wordpress".toUpperCase()) > -1)
+        return "fa-wordpress";
+      else if (filterName.indexOf("sql".toUpperCase()) > -1)
+        return "fas fa-database";
+      else if (filterName.indexOf("backend".toUpperCase()) > -1)
+        return "fas fa-terminal";
       else if (filterName.indexOf("frontend".toUpperCase()) > -1)
         return "far fa-window-maximize";
       return "fas fa-pencil-alt";
@@ -196,8 +224,9 @@ i {
         font-weight: bold;
         text-decoration: underline;
         text-align: center;
-        font-family: sf pro display, -apple-system, BlinkMacSystemFont, Roboto, segoe ui,
-          Helvetica, Arial, sans-serif, apple color emoji, segoe ui emoji, segoe ui symbol;
+        font-family: sf pro display, -apple-system, BlinkMacSystemFont, Roboto,
+          segoe ui, Helvetica, Arial, sans-serif, apple color emoji,
+          segoe ui emoji, segoe ui symbol;
         padding: 0 2rem;
         width: 73%;
         position: relative;
