49bf466b72757017fd06ae2b8bf56f1617f51bec
alXDraghiciu <88316605+alXDraghiciu@users.noreply.github.com>
2021-09-28 15:50:05 +0300

    added dropdown to host form and animations

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -8,29 +8,6 @@
           <button @click="loadData">Refresh</button>
         </div>
       </div>
-      <form @submit.prevent.stop="submitEdit" class="vld-parent" ref="loaderForm">
-        <h3>{{ !selectedUser ? "Select a user:" : "User: " + selectedUser.email }}</h3>
-        <div class="fields" v-if="selectedUser">
-          <label style="display: block">Roles:</label>
-          <input type="checkbox" value="User" v-model="selectedUser.roles" />
-          <label for="jack">User 😩</label>
-          <input type="checkbox" value="Expert" v-model="selectedUser.roles" />
-          <label for="john">Expert ⭐</label>
-          <input type="checkbox" value="Admin" v-model="selectedUser.roles" />
-          <label for="mike">Admin 🔧</label>
-        </div>
-        <div class="button-row">
-          <input
-            type="submit"
-            value="Save Changes"
-            v-if="selectedUser"
-            :disabled="selectedUser.roles.lenght == 0"
-          />
-          <button type="button" v-if="selectedUser" @click="selectedUser = null">
-            Cancel
-          </button>
-        </div>
-      </form>
     </header>
     <section>
       <table>
@@ -42,12 +19,12 @@
         <tr
           v-for="user in users"
           :key="user.id"
-          @click="selectUser(user)"
+          @click="selectUser(user, $event)"
           :class="{ selected: selectedUser == user }"
         >
           <td>{{ user.id }}</td>
           <td>{{ user.email }}</td>
-          <td>
+          <td class="roles">
             <p
               v-for="role in user.roles"
               :key="role"
@@ -66,6 +43,81 @@
         </tr>
       </table>
     </section>
+    <div
+      class="dropdown-menu"
+      :style="[
+        { top: dropdownPosition?.y + 'px', left: dropdownPosition?.x + 'px' },
+        dropdownPosition?.x ? { opacity: '100%' } : { opacity: '0' },
+      ]"
+    >
+      <div class="dropdown-message" v-if="dropdownMessage">
+        <p>{{ dropdownMessage }}</p>
+      </div>
+
+      <form @submit.prevent.stop="submitEdit" class="vld-parent" ref="loaderForm">
+        <h3>{{ !selectedUser ? "Select a user:" : "User: " + selectedUser.email }}</h3>
+        <div class="fields" v-if="selectedUser">
+          <label style="display: block">Roles:</label>
+          <input type="checkbox" value="User" v-model="selectedUser.roles" />
+          <label for="jack">User 😩</label>
+          <input type="checkbox" value="Expert" v-model="selectedUser.roles" />
+          <label for="john">Expert ⭐</label>
+          <input type="checkbox" value="Admin" v-model="selectedUser.roles" />
+          <label for="mike">Admin 🔧</label>
+        </div>
+        <div class="button-row">
+          <div
+            :style="[
+              !confirm
+                ? { opacity: '100%', pointerEvents: 'auto' }
+                : { opacity: '0', pointerEvents: 'none' },
+              ,
+              { transition: '0.2s opacity ease-in-out' },
+            ]"
+          >
+            <input
+              type="submit"
+              value="Save Changes"
+              :disabled="selectedUser?.roles.lenght == 0"
+            />
+            <button
+              type="button"
+              class="warn-button"
+              @click.prevent.stop="confirm = true"
+            >
+              Delete
+            </button>
+            <button
+              type="button"
+              class="warn-button"
+              @click.prevent.stop="cancelSelection"
+            >
+              Cancel
+            </button>
+          </div>
+          <div
+            class="confirm-buttons"
+            :style="[
+              confirm
+                ? { opacity: '100%', pointerEvents: 'auto' }
+                : { opacity: '0', pointerEvents: 'none' },
+              ,
+              { transition: '0.2s opacity ease-in-out' },
+            ]"
+          >
+            <h4>Are you sure?</h4>
+            <button type="button" @click.prevent.stop="confirm = true">Yes</button>
+            <button
+              type="button"
+              class="warn-button"
+              @click.prevent.stop="confirm = false"
+            >
+              No
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
   </main>
 </template>
 
@@ -76,10 +128,17 @@ export default {
       selectedUser: null,
       users: [],
       loader: undefined,
+      dropdownPosition: {
+        x: null,
+        y: null,
+      },
+      dropdownMessage: "",
+      confirm: false,
     };
   },
   methods: {
     loadData() {
+      this.cancelSelection();
       this.loader = this.$loading.show({
         container: this.$refs.loaderParent,
         isFullPage: false,
@@ -96,7 +155,13 @@ export default {
           this.loader = undefined;
         });
     },
-    selectUser(user) {
+    selectUser(user, event) {
+      if (this.dropdownMessage) return;
+      if (user?.id != this.selectedUser?.id) {
+        this.confirm = false;
+        this.dropdownPosition.x = event.pageX;
+        this.dropdownPosition.y = event.pageY;
+      }
       this.selectedUser = JSON.parse(JSON.stringify(user));
     },
     submitEdit() {
@@ -110,13 +175,40 @@ export default {
       this.$http
         .post(`/users/${this.selectedUser.id}/roles`, roles)
         .then((results) => {
+          this.dropdownMessage = "Roles edited successfully!";
+          setTimeout(() => {
+            this.dropdownMessage = "";
+            this.cancelSelection();
+          }, 2000);
           console.log("Success!");
-          this.selectedUser = null;
+        })
+        .catch((error) => {
+          switch (error.response.status) {
+            case 400:
+              this.dropdownMessage = "Invalid roles.";
+              break;
+            case 404:
+              this.dropdownMessage = "Oops! User was deleted.";
+              break;
+            default:
+              break;
+          }
+
+          setTimeout(() => {
+            this.dropdownMessage = "";
+          }, 2000);
         })
         .finally(() => {
           this.loader.hide();
           this.loader = undefined;
         });
+    },
+    cancelSelection() {
+      this.dropdownPosition.x = null;
+      this.dropdownPosition.y = null;
+      setTimeout(() => {
+        this.selectedUser = null;
+      }, 200);
     },
   },
   created() {
@@ -126,23 +218,67 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.dropdown-menu {
+  position: fixed;
+  background-color: white;
+  border-radius: 30px;
+  box-shadow: 0px 1px 5px rgba(0, 0, 0, 0.26);
+  padding: 1rem;
+  transition: 0.2s opacity ease-in-out, 0.1s top ease-in-out, 0.1s left ease-in-out;
+  z-index: 100;
+  form {
+    padding: 0 1rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+    height: 100%;
+    button,
+    input[type="submit"] {
+      width: auto;
+      display: inline-block;
+    }
+  }
+  .dropdown-message {
+    border-radius: 30px;
+    height: 100%;
+    text-align: center;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    display: block;
+    margin: 0;
+    overflow: clip;
+    background-color: rgba(255, 255, 255, 0.9);
+    font-size: larger;
+    font-weight: bolder;
+    font-family: $f-u-bm;
+    z-index: 101;
+    display: flex;
+    align-items: center;
+    p {
+      margin: auto;
+    }
+  }
+}
 .button-row {
+  position: relative;
   margin: 1rem 0;
+  > .confirm-buttons {
+    h4 {
+      margin: 0;
+      font-weight: bolder;
+      font-family: $f-u-bm;
+    }
+    top: 0;
+    left: 0;
+    position: absolute;
+    width: 100%;
+    text-align: center;
+  }
 }
 .selected {
   background-color: rgb(232, 208, 255);
-}
-form {
-  padding: 0 1rem;
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-end;
-  height: 100%;
-  button,
-  input[type="submit"] {
-    width: auto;
-    display: inline-block;
-  }
 }
 header {
   height: 12rem;
@@ -163,34 +299,37 @@ header {
     padding: 1rem;
     height: auto;
   }
-  button,
-  input[type="submit"] {
-    height: 2rem;
-    border-radius: 25px;
-    font-family: $f-u-bm;
-    padding: 0 1rem;
-    background-color: $c-u-pur;
-    color: white;
-    border: 0;
-    margin: 0 1rem;
-    box-shadow: 0 2px 11px 1px rgb(0 0 0 / 26%);
-    cursor: pointer;
-    &:hover,
-    :active,
-    :focus-visible {
-      box-shadow: 0 2px 7px 4px rgb(0 0 0 / 26%);
-      background-color: yellowgreen;
-    }
-    &:disabled {
-      background-color: transparent !important;
-      box-shadow: inherit !important;
-      color: gray !important;
-      cursor: default;
-    }
-  }
 }
 h1 {
   margin: 0;
+}
+button,
+input[type="submit"] {
+  height: 2rem;
+  border-radius: 25px;
+  font-family: $f-u-bm;
+  padding: 0 1rem;
+  background-color: $c-u-pur;
+  color: white;
+  border: 0;
+  margin: 0 1rem;
+  box-shadow: 0 2px 11px 1px rgb(0 0 0 / 26%);
+  cursor: pointer;
+  &:hover,
+  :active,
+  :focus-visible {
+    box-shadow: 0 2px 7px 4px rgb(0 0 0 / 26%);
+    background-color: yellowgreen;
+  }
+  &:disabled {
+    background-color: transparent !important;
+    box-shadow: inherit !important;
+    color: gray !important;
+    cursor: default;
+  }
+}
+.warn-button {
+  background-color: brown;
 }
 table {
   width: 100%;
@@ -209,15 +348,7 @@ tr {
     background-color: rgb(232, 208, 255);
   }
   position: relative;
-}
-
-.dropdown-menu {
-  column-span: all;
-  //   opacity: 0;
-  position: absolute;
-}
-.dropdown-menu .active {
-  opacity: 100%;
+  cursor: pointer;
 }
 
 .role {
@@ -237,5 +368,9 @@ tr {
 }
 .role-user {
   background-color: indianred;
+}
+
+.roles {
+  display: flex;
 }
 </style>
